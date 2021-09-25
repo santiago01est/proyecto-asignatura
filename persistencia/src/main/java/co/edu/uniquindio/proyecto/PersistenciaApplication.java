76ee768d85b53612188cdccd93a2daf8bbf2c905
santiago <santiago01est@gmail.com>
2021-09-25 01:13:33 -0500

    creo archivo readme y gitignore

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/PersistenciaApplication.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/PersistenciaApplication.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.proyecto;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,3 +11,4 @@ public class PersistenciaApplication {
     }
 
 }
+ 
